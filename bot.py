ae50a87ebd542896edae036328237dc1c672c4d9
Stan26-2020 <115564361+Stan26-2020@users.noreply.github.com>
2025-08-04 15:12:28 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-APP_URL = os.getenv("RENDER_URL")
+APP_URL = os.getenv("https://upscalermagicbot.onrender.com")
 
 async def main():
     await app.bot.set_webhook(url=APP_URL + f"/{TOKEN}")
@@ -6,4 +6,5 @@ async def main():
         listen="0.0.0.0",
         port=int(os.getenv("PORT", 10000)),
         webhook_url=APP_URL + f"/{TOKEN}"
+
     )
